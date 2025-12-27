25af6d047a174137e8b966b92627f8d3111aa152
Jobin Sebastian <jobsebastianjobin@gmail.com>
2025-12-27 14:00:08 +0530

    Add startup script for Railway deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py migrate --noinput && python manage.py collectstatic --noinput && gunicorn config.wsgi --log-file -
+web: bash start.sh
---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Starting Gunicorn..."
+exec gunicorn config.wsgi --bind 0.0.0.0:${PORT:-8000} --log-file -
